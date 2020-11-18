520ffa767c4ddfa40f5b3d9a835039d4d4779003
schaefm <matt.schaeffer@autodesk.com>
2020-11-18 11:36:14 -0700

    Migration logic

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
 import AppRoot from "./components/appRoot.js";
 
 class Main{
@@ -7,7 +6,9 @@ class Main{
         const appRoot = React.createElement(AppRoot, {
         }, null);
 
-        ReactDOM.render(appRoot, domContainer);
+        this.migratePlugins(() => {
+            ReactDOM.render(appRoot, domContainer);
+        });
 
         document.getElementById('LearnMoreLink').addEventListener('click', () => {
             FormItInterface.CallMethod("FormIt.OpenURL", 'https://formit3d.github.io/FormItExamplePlugins/index.html');
@@ -16,6 +17,58 @@ class Main{
         document.getElementById('BuildLink').addEventListener('click', () => {
             FormItInterface.CallMethod("FormIt.OpenURL", 'https://formit3d.github.io/FormItExamplePlugins/docs/HowToBuild.html');
         });
+    }
+
+    //This migrates plugins from their previous git structure so clients stay in sync with latest plugin.
+    migratePlugins(callback){
+        if (localStorage.getItem('hasMigrated')){
+            callback();
+            return;
+        }
+
+        const migrationMap = {
+            "https://formit3d.github.io/FormItWorkflowPlugins/RebuildCurve": "https://FormIt3D.github.io/RebuildCurve",
+            "https://formit3d.github.io/FormItWorkflowPlugins/ValidateCleanModel": "https://FormIt3D.github.io/ValidateCleanModel",
+            "https://formit3d.github.io/FormItWorkflowPlugins/StringLightGenerator": "https://FormIt3D.github.io/StringLightGenerator",
+            "https://formit3d.github.io/FormItWorkflowPlugins/PropertiesPlus": "https://FormIt3D.github.io/PropertiesPlus",
+            "https://formit3d.github.io/FormItWorkflowPlugins/ManageCameras": "https://FormIt3D.github.io/ManageCameras",
+            "https://formit3d.github.io/FormItWorkflowPlugins/GenerateVertex": "https://FormIt3D.github.io/GenerateVertex",
+            "https://formit3d.github.io/FormItWorkflowPlugins/FlipAlong": "https://FormIt3D.github.io/FlipAlong",
+            "https://formit3d.github.io/FormItWorkflowPlugins/FilletCorner": "https://FormIt3D.github.io/FilletCorner",
+            "https://formit3d.github.io/FormItWorkflowPlugins/ExtractMaterialTextures": "https://FormIt3D.github.io/ExtractMaterialTextures",
+            "https://formit3d.github.io/FormItWorkflowPlugins/MeshUnmeshAll": "https://FormIt3D.github.io/MeshUnmeshAll"
+        }
+
+        FormItInterface.CallMethod("FormIt.GetInstalledPlugins", "", (installedPlugins) => {
+            installedPlugins = JSON.parse(installedPlugins).filter(p => p);
+
+            const migrationPromises = [];
+
+            console.log(1)
+            installedPlugins.forEach((installedPluginPath) => {
+
+                const migrationPath = migrationMap[installedPluginPath]
+
+                if (migrationPath){
+                    const migratePromise = new Promise((resolve,reject) => {
+                        console.log('in promise')
+                        FormItInterface.CallMethod("FormIt.UninstallPlugin", installedPluginPath, () => {
+                            FormItInterface.CallMethod("FormIt.InstallPlugin", migrationPath, () => {
+                                resolve();
+                            });   
+                        });
+                    });
+
+                    migrationPromises.push(migratePromise);  
+                }
+
+                Promise.all(migrationPromises).then(() => {
+                    callback();
+                });
+            });
+        });
+
+        localStorage.setItem('hasMigrated', 'true');
     }
 }
 
