87c153d0e1f145f239f075ac8c2044c78d88e840
schaefm <matt.schaeffer@autodesk.com>
2021-01-11 15:21:17 -0700

    cleanup

--- components/appRoot.js
+++ components/appRoot.js
@@ -33,8 +33,6 @@ class AppRoot extends React.Component {
                 q: 'topic:formit-plugin-recommended'//&sort=stars&order=desc',
             });
 
-            console.log(recommendedPluginsResult)
-
             const publicPlugins = publicPluginsResult.data.items.filter((repo) => {
                 return repo.has_pages;
             });
@@ -46,8 +44,6 @@ class AppRoot extends React.Component {
 
                 return hasPages && isApproved;
             });
-
-            console.log(recommendedPlugins)
 
             const needsApprovalPlugins = recommendedPluginsResult.data.items.filter((repo) => {
                 const hasPages = repo.has_pages;
