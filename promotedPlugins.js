582b3267b09d27936b11754f255002d74f8a7230
Josh Goldstein <josh.goldstein@autodesk.com>
2021-11-10 09:30:27 -0700

    Adding Matterlab's 3D Context plugin to promoted list

--- promotedPlugins.js
+++ promotedPlugins.js
@@ -1,4 +1,5 @@
 const promotedPlugins = [
+    "https://github.com/matterlab-co/FormIt-Context-Plugin",
     "https://github.com/FormIt3D/PluginPlayground",
     "https://github.com/FormIt3D/Lumion"
 ];
