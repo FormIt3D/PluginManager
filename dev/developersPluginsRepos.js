bfb12341b0ef17046d12818159e5aa26bf053c08
Brett Garrison <brett.garrison@autodesk.com>
2022-01-14 10:55:54 -0700

    FORMIT-12192 Adds Vue plugin and shortens description to 280 chars

--- dev/developersPluginsRepos.js
+++ dev/developersPluginsRepos.js
@@ -1,6 +1,7 @@
 const developersPluginsRepos = [
     "FormIt3D/Messages",
-    "FormIt3D/PluginPlayground"
+    "FormIt3D/PluginPlayground",
+    "CuninghamDev/Hello-Vue-FormIt-Plugin"
 ]
 
 export default developersPluginsRepos;
